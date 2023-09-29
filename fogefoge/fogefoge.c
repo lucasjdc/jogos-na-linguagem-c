000b530bc54eecb2f09b1395221d9233a5b7e2cc
lucasjdc <lucasjpdc@gmail.com>
2023-09-29 17:34:39 -0300

    alocação dinâmica de memória

--- fogefoge/fogefoge.c
+++ fogefoge/fogefoge.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** mapa;
+int linhas;
+int colunas;
+
 int main() {
-  char mapa[5][10+1];
+  
 
   FILE *f;
   f = fopen("mapa.txt", "r");
   if (f == 0) {
     printf("Erro na leitura do mapa\n");
     exit(1);
+  }
+  
+  fscanf(f, "%d %d", &linhas, &colunas);
+  printf("linhas %d colunas %d\n", linhas, colunas);
+  
+  mapa = malloc(sizeof(char*) * linhas);
+  for(int i = 0; i < linhas; i++) {
+	  mapa[i] = malloc(sizeof(char) * (colunas+1));
   }
 
   // lendo o arquivo mapa.txt
@@ -22,4 +34,8 @@ int main() {
   }
 
   fclose(f);
+  for(int i = 0; i < linhas; i++){
+	  free(mapa[i]);
+  }
+  free(mapa);
 }
