2b9db5388f793a0e604cddabbc107728ca2753da
Lucas Costa <lucasjpdc@gmail.com>
2023-09-30 12:03:24 -0300

    Tratando as paredes e os extremos do jogo

--- fogefoge/fogefoge.c
+++ fogefoge/fogefoge.c
@@ -12,30 +12,39 @@ int acabou(){
 
 void move(char direcao){
 
-	// quando só tem uma instrução no if não e necessario usar {}
 	if (direcao != 'a' && direcao != 'w' && direcao != 's' && direcao != 'd')
 		return;
-	
-	m.matriz[heroi.x][heroi.y] = '.';
 
-	switch(direcao){
+	int proximox = heroi.x;
+	int proximoy = heroi.y;	
+	
+
+	switch(direcao) {
 		case 'a':
-			m.matriz[heroi.x][heroi.y-1] = '@';
-			heroi.y--;
+			proximoy--;
 			break;
 		case 'w':
-			m.matriz[heroi.x-1][heroi.y] = '@';
-			heroi.x--;
+			proximox--;
 			break;
-		case 's':
-			m.matriz[heroi.x+1][heroi.y] = '@';
-			heroi.x++;
+		case 's':			
+			proximox++;
 			break;
-		case 'd':
-			m.matriz[heroi.x][heroi.y+1] = '@';
-			heroi.y++;
+		case 'd':			
+			proximoy++;
 			break;
 	}
+
+	if(proximox >= m.linhas)
+		return;
+	if(proximoy >= m.colunas)
+		return;
+	if(m.matriz[proximox][proximoy] != '.')
+		return;
+
+	m.matriz[proximox][proximoy] = '@';
+	m.matriz[heroi.x][heroi.y] = '.';
+	heroi.x = proximox;
+	heroi.y = proximoy;
 	
 	
 }
