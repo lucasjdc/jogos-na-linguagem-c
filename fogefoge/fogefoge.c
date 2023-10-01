06a0fb7e6699969ee9a3e80245775228353786e7
Lucas Costa <lucasjpdc@gmail.com>
2023-10-01 09:43:51 -0300

    Melhorando o algoritmo recursivo

--- fogefoge/fogefoge.c
+++ fogefoge/fogefoge.c
@@ -99,7 +99,11 @@ void move(char direcao){
 }
 
 void explodepilula(int x, int y, int qtd) {
+	
 	if (qtd == 0) return;
+	if(!ehvalida(&m, x, y+1)) return;
+	if(ehparede(&m, x, y+1)) return;
+
 	m.matriz[x][y+1] = VAZIO;
 	explodepilula(x, y+1, qtd - 1);
 }
