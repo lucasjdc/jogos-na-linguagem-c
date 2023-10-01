32073d47d5fbae81f9fce5ba692f4f4c04695785
lucasjdc <lucasjpdc@gmail.com>
2023-09-30 22:42:38 -0300

    Começando a destruir os fantasmas

--- fogefoge/fogefoge.c
+++ fogefoge/fogefoge.c
@@ -99,7 +99,14 @@ void move(char direcao){
 }
 
 void explodepilula() {
-	printf("explodiu");
+	for(int i = 1; i  <=3; i++) {
+		if(ehvalida(&m, heroi.x, heroi.y+i)) {
+			if(ehparede(&m, heroi.x, heroi.y=i)) {
+				break;
+			}
+			m.matriz[heroi.x][heroi.y] = VAZIO;
+		}
+	}
 }
 
 int main() {
