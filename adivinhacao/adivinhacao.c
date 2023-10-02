69e0bf13f0667fce10cb96d374de88683ec87172
lucasjdc <lucasjpdc@gmail.com>
2023-10-02 16:41:21 -0300

    If e else

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -10,7 +10,23 @@ int main() {
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
 
-    printf("\nEscolha um número\n");
+    int numerosecreto = 42;
+    int chute;
+    printf("\nQual é o seu chute?\n");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d.\n", chute);
+    if (chute == numerosecreto) {
+    	printf("Parabéns! Você acertou!\n");
+    	printf("Jogue de novo, você é um bom jogador!\n");
+    } else {
+    	if (chute > numerosecreto) {
+    		printf("Seu chute foi maior que o número secreto!\n");
+    	}
+    	
+    	if (chute < numerosecreto) {
+    		printf("Seu chute foi menor que o número secreto!\n");
+    	}
+    }
 
     return 0;
 }
