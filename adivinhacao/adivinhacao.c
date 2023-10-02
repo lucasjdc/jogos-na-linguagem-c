15abec393ad0d236195f3425051c21415471c689
lucasjdc <lucasjpdc@gmail.com>
2023-10-02 16:57:30 -0300

    Variações no código

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -15,15 +15,19 @@ int main() {
     printf("\nQual é o seu chute?\n");
     scanf("%d", &chute);
     printf("Seu chute foi %d.\n", chute);
-    if (chute == numerosecreto) {
+   
+    int acertou = (chute == numerosecreto);
+
+    if (acertou) {
     	printf("Parabéns! Você acertou!\n");
     	printf("Jogue de novo, você é um bom jogador!\n");
     } else {
-    	if (chute > numerosecreto) {
+
+    	int maior = (chute > numerosecreto);
+
+    	if (maior) {
     		printf("Seu chute foi maior que o número secreto!\n");
-    	}
-    	
-    	if (chute < numerosecreto) {
+    	} else {
     		printf("Seu chute foi menor que o número secreto!\n");
     	}
     }
