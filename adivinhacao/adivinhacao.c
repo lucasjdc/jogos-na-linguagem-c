0460c79d8d84e5b57aa0ff87f0cfde8d97b52cbd
lucasjdc <lucasjpdc@gmail.com>
2023-10-01 22:13:40 -0300

    corrigindo configurações do windows

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Bem vindo ao nosso jogo de adivinhação\n");
+    
+	system("chcp 65001");   // coloca o prompt de comando em UTF-8
+	system("cls");          // limpa a tela
+
+	printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    printf("\nEscolha um número\n");
+
+    return 0;
 }
